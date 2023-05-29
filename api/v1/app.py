5ac61c8e10567d941c571a71a123918529b3260a
frankkib <frankkibagendi13@gmail.com>
2023-05-29 08:25:18 +0000

    creating an error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for the application"""
 from flask import Flask
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 app = Flask(__name__)
@@ -11,6 +12,12 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_appcontext(exception):
     """tears down the app contets"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """returns not found error"""
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
