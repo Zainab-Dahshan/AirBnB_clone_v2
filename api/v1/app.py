20d25a377571bc8eac4917c5dd6b8d96ef17afcc
frankkib <frankkibagendi13@gmail.com>
2023-05-29 07:57:59 +0000

    starting the api

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,9 @@
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
- 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
